b0d4bf52ed5fbb3503677068ddceb287d273e44f
MG <mg-git.plumbic@8shield.net>
2021-11-02 20:09:41 +0800

    Create dashboard.php
    Add navbar structure

---
+++ dashboard.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+    <title>SwiftLaundry</title>
+</head>
+<body>
+    <header> 
+        <div class="brand-name">
+            <h3>SwiftLaundry</h3>
+        </div>
+        <div class="logout-area">
+            <a href="#" class="logout-button">Logout</a>
+        </div>
+    </header>
+
+    <nav>
+        <center>
+            <a href="#"><i class="far fa-chart-bar"></i><span>Overview</span></a>
+            <a href="#"><i class="fas fa-dolly-flatbed"></i><span>Items</span></a>
+            <a href="#"><i class="fas fa-receipt"></i><span>Receipts</span></a>
+            <a href="#"><i class="fas fa-ticket-alt"></i><span>Tickets</span></a>
+            <a href="#"><i class="fas fa-clipboard-list"></i><span>Reports</span></a>
+        </center>
+    </nav>
+
+
+</body>
+</html>
