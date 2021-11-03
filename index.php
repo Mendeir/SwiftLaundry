e917bca5d5c7fa800d3e8b6e963cb9a56d4e8842
dbornilla <78960805+dbornilla@users.noreply.github.com>
2021-11-03 14:09:15 +0800

    Login with image
    added image

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+	<title> Swift Laundry </title>
+
+	<link rel="stylesheet" href="css/StyleLoginPage.css" />
+	
+</head>
+
+<body>
+
+	<center>
+		<h1> Swift Laundry</h1>
+		
+		<form action="Technical3_B_3.php" method="get">
+			<h2>Log-in</h2>
+			<div class='first'>
+				<label for="username"> <b> Username:</b> </label>
+				<input type="text" placeholder="Enter your username" name="uname" required>
+				<br>
+				<label for="pass"> <b>Password:</b> </label>
+				<input type="password" placeholder="Enter your password" name="pword" required>
+				<br>
+				<button type="submit"> Login </button>
+			</div>
+		</form>
+
+	</center>
+
+</body>
+</html>
