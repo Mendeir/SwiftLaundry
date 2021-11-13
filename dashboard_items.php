0f1fd8762b7c90b579472fd2e9ecfd4671c2fdcc
MG <mg-git.plumbic@8shield.net>
2021-11-13 14:12:09 +0800

    Implement item card

---
+++ dashboard_items.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+    <link rel = "stylesheet" type = "text/css" href="css/dashboard_style.css">
+    <title>Swift Laundry</title>
+</head>
+<body>
+    <header> 
+        <div class="brand-name">
+            <h3>SwiftLaundry</h3>
+        </div>
+        <div class="logout-area">
+            <a href="#" class="logout-button">Logout</a>
+            <span>USER</span>
+        </div>
+    </header>
+
+    <nav>
+            <a href="#"><i class="far fa-chart-bar"></i><span>Overview</span></a>
+            <a href="#"><i class="fas fa-dolly-flatbed"></i><span>Items</span></a>
+            <a href="#"><i class="fas fa-receipt"></i><span>Receipts</span></a>
+            <a href="#"><i class="fas fa-ticket-alt"></i><span>Tickets</span></a>
+            <a href="#"><i class="fas fa-clipboard-list"></i><span>Reports</span></a>
+    </nav>
+
+    <section>
+        <div class="content">
+            <div class="content-header">
+                <div class="section-name">
+                    <h3>ITEMS</h3>
+                </div>
+                <div class="add-item">
+                    <a href="#" class="add-item-button">Add Item +</a>
+                </div>
+
+            </div>
+            
+            <div class="content-items">
+                <div class="item-container">
+                    <div class="card">
+                        <div class="product-name">Test 1</div>
+                        <div class="image"><img src="img/items/test.jpg" alt="Item"></div>
+                        <div class="price">Php 1000</div>
+                    </div>
+
+                    <div class="card">
+                        <div class="product-name">Test 1</div>
+                        <div class="image"><img src="img/items/test.jpg" alt="Item"></div>
+                        <div class="price">Php 1000</div>
+                    </div>
+
+                    <div class="card">
+                        <div class="product-name">Test 1</div>
+                        <div class="image"><img src="img/items/test.jpg" alt="Item"></div>
+                        <div class="price">Php 1000</div>
+                    </div>
+
+                    <div class="card">
+                        <div class="product-name">Test 1</div>
+                        <div class="image"><img src="img/items/test.jpg" alt="Item"></div>
+                        <div class="price">Php 1000</div>
+                    </div>
+
+                    <div class="card">
+                        <div class="product-name">Test 1</div>
+                        <div class="image"><img src="img/items/test.jpg" alt="Item"></div>
+                        <div class="price">Php 1000</div>
+                    </div>
+
+                    <div class="card">
+                        <div class="product-name">Test 1</div>
+                        <div class="image"><img src="img/items/test.jpg" alt="Item"></div>
+                        <div class="price">Php 1000</div>
+                    </div>
+
+                    <div class="card">
+                        <div class="product-name">Test 1</div>
+                        <div class="image"><img src="img/items/test.jpg" alt="Item"></div>
+                        <div class="price">Php 1000</div>
+                    </div>
+
+                    <div class="card">
+                        <div class="product-name">Test 1</div>
+                        <div class="image"><img src="img/items/test.jpg" alt="Item"></div>
+                        <div class="price">Php 1000</div>
+                    </div>
+
+                    <div class="card">
+                        <div class="product-name">Test 1</div>
+                        <div class="image"><img src="img/items/test.jpg" alt="Item"></div>
+                        <div class="price">Php 1000</div>
+                    </div>
+
+                    <div class="card">
+                        <div class="product-name">Test 1</div>
+                        <div class="image"><img src="img/items/test.jpg" alt="Item"></div>
+                        <div class="price">Php 1000</div>
+                    </div>
+
+                    <div class="card">
+                        <div class="product-name">Test 1</div>
+                        <div class="image"><img src="img/items/test.jpg" alt="Item"></div>
+                        <div class="price">Php 1000</div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+</body>
+</html>
