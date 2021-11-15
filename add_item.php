0ff6bd53e37ac3a17ce298d9eb7c58dc85ccddae
MG <mg-git.plumbic@8shield.net>
2021-11-15 21:03:57 +0800

    Implement add_item button

---
+++ add_item.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once("inc/head.inc") ?>
+</head>
+<body>
+    <?php include_once("inc/header.inc") ?>    
+
+    <section>
+        <div class="content">
+            <div class="section-name">
+                    <h3>Add Items</h3>
+                </div>
+                <div class="add-item">
+                    <a href="dashboard_items.php" class="add-item-button">Back</a>
+                </div>
+
+            <div class="content-items">
+                <form action="dashboard_items.php" class="add-item-form">
+                    <label for="item-name"> Item Name: </label>
+                    <input type="text" id="item-name" name="item-name" required>
+
+                    <label for="price"> Price (Php): </label>
+                    <input type="number" id="price" name="price" min="0.00" max="10000.00" step="5" required>
+
+                    <label for="image-path"> Image Path: </label>
+                    <input type="file" id="image-path" name="image-path" accept="image/png, image/jpeg" required>
+                    
+                    <input type="submit" id="add-item-button" name="submit" value="Add Item">
+                </form>
+            </div>
+        </div>
+
+
+        
+    </section>
+
+
+</body>
+</html>
